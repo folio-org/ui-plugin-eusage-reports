23244756f31911d6513c219a143472e155b420a0
Mike Taylor <mike@indexdata.com>
2021-04-09 17:46:13 +0100

    First stylistically adequate version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-export default () => 'plugin goes here';
+import { FormattedMessage } from 'react-intl';
+import { Accordion } from '@folio/stripes/components';
+
+const PluginEusageReports = () => {
+  return (
+    <Accordion
+      id="plugin-eusage-reports"
+      label={<FormattedMessage id="ui-plugin-eusage-reports.accordion.label" />}
+      closedByDefault
+    >
+      Visualization Plugin goes here.
+    </Accordion>
+  );
+};
+
+export default PluginEusageReports;
