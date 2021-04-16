948e4bab022323455e65c948c966b5a18aebe551
Mike Taylor <mike@indexdata.com>
2021-04-16 15:32:41 +0100

    Add useful-tests directory

---
+++ useful-test/README.md
@@ -0,0 +1,3 @@
+This directory contains tests that I found useful when actually
+building the module, as opposed to the tests required to get it
+through QA.
---
+++ useful-test/test.js
@@ -0,0 +1,12 @@
+import compileCostPerUseData from '../src/reports/CostPerUse/compile.js';
+
+const csv = [
+  ['Agreement line', 'Reporting year', 'Cost per request - total', 'Cost per request - unique'],
+  ['Line A', '2017', '23.45', '25.22'],
+  ['Line A', '2018', '28.02', '29.31'],
+  ['Line B', '2018', '3.45', '8.92'],
+  ['Line B', '2019', '3.76', '9.00'],
+  ['Line C', '2017', '7.67', '11.42'],
+];
+
+console.log(compileCostPerUseData(csv));
