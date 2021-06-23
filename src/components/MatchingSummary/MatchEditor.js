57a5e9fbcaaa7e640558e018d00fc92a376ba675
Mike Taylor <mike@indexdata.com>
2021-06-23 18:43:39 +0100

    Infuriating giant commit that does nothing but reindent

--- src/components/MatchingSummary/MatchEditor.js
+++ src/components/MatchingSummary/MatchEditor.js
@@ -12,62 +12,62 @@ function MatchEditor({ matchType, onClose, data, paneTitleRef }) {
 
   return (
     <HasCommand commands={[{ name: 'close', handler: onClose }]}>
-    <Paneset isRoot>
-      <Pane
-        defaultWidth="fill"
-        dismissible
-        onClose={onClose}
-        paneTitle={<FormattedMessage
-          id="ui-plugin-eusage-reports.matching-summary.matcher-heading"
-          values={{ label: data.usageDataProvider.label }}
-        />}
-        paneTitleRef={paneTitleRef}
-      >
-        <Layout className="textCentered">
-          <ButtonGroup>
-            {
-              categories.map(cat => (
-                <Button
-                  key={cat.key}
-                  buttonStyle={`${cat.key === currentMatchType ? 'primary' : 'default'}`}
-                  id={`segment-category-${cat.key}`}
-                  onClick={() => setCurrentMatchType(cat.key)}
-                >
-                  <FormattedMessage id={`ui-plugin-eusage-reports.matching-summary.${cat.key}`} />
-                </Button>
-              ))
-            }
-          </ButtonGroup>
-        </Layout>
+      <Paneset isRoot>
+        <Pane
+          defaultWidth="fill"
+          dismissible
+          onClose={onClose}
+          paneTitle={<FormattedMessage
+            id="ui-plugin-eusage-reports.matching-summary.matcher-heading"
+            values={{ label: data.usageDataProvider.label }}
+          />}
+          paneTitleRef={paneTitleRef}
+        >
+          <Layout className="textCentered">
+            <ButtonGroup>
+              {
+                categories.map(cat => (
+                  <Button
+                    key={cat.key}
+                    buttonStyle={`${cat.key === currentMatchType ? 'primary' : 'default'}`}
+                    id={`segment-category-${cat.key}`}
+                    onClick={() => setCurrentMatchType(cat.key)}
+                  >
+                    <FormattedMessage id={`ui-plugin-eusage-reports.matching-summary.${cat.key}`} />
+                  </Button>
+                ))
+              }
+            </ButtonGroup>
+          </Layout>
 
-        <MultiColumnList
-          autosize
-          contentData={dataSet}
-          visibleColumns={['id', 'counterReportTitle', 'kbTitleName', 'kbTitleId', 'kbManualMatch', 'action']}
-          columnMapping={{
-            id: <FormattedMessage id="ui-plugin-eusage-reports.column.id" />,
-            counterReportTitle: <FormattedMessage id="ui-plugin-eusage-reports.column.counterReportTitle" />,
-            kbTitleName: <FormattedMessage id="ui-plugin-eusage-reports.column.kbTitleName" />,
-            kbTitleId: <FormattedMessage id="ui-plugin-eusage-reports.column.kbTitleId" />,
-            kbManualMatch: <FormattedMessage id="ui-plugin-eusage-reports.column.kbManualMatch" />,
-            action: <FormattedMessage id="ui-plugin-eusage-reports.column.action" />,
-          }}
-          columnWidths={{
-            id: '90px',
-            counterReportTitle: '300px',
-            kbTitleName: '300px',
-            kbTitleId: '90px',
-            kbManualMatch: '120px',
-            action: '100px',
-          }}
-          formatter={{
-            id: r => r.id.substring(0, 8),
-            kbTitleId: r => (r.kbTitleId || '').substring(0, 8),
-            action: () => '...',
-          }}
-        />
-      </Pane>
-    </Paneset>
+          <MultiColumnList
+            autosize
+            contentData={dataSet}
+            visibleColumns={['id', 'counterReportTitle', 'kbTitleName', 'kbTitleId', 'kbManualMatch', 'action']}
+            columnMapping={{
+              id: <FormattedMessage id="ui-plugin-eusage-reports.column.id" />,
+              counterReportTitle: <FormattedMessage id="ui-plugin-eusage-reports.column.counterReportTitle" />,
+              kbTitleName: <FormattedMessage id="ui-plugin-eusage-reports.column.kbTitleName" />,
+              kbTitleId: <FormattedMessage id="ui-plugin-eusage-reports.column.kbTitleId" />,
+              kbManualMatch: <FormattedMessage id="ui-plugin-eusage-reports.column.kbManualMatch" />,
+              action: <FormattedMessage id="ui-plugin-eusage-reports.column.action" />,
+            }}
+            columnWidths={{
+              id: '90px',
+              counterReportTitle: '300px',
+              kbTitleName: '300px',
+              kbTitleId: '90px',
+              kbManualMatch: '120px',
+              action: '100px',
+            }}
+            formatter={{
+              id: r => r.id.substring(0, 8),
+              kbTitleId: r => (r.kbTitleId || '').substring(0, 8),
+              action: () => '...',
+            }}
+          />
+        </Pane>
+      </Paneset>
     </HasCommand>
   );
 }
