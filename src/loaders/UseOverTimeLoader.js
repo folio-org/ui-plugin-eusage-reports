499205dcd972f48ea6f8f945255fe8fcad61352b
Mike Taylor <mike@indexdata.com>
2021-07-21 16:40:22 +0100

    Use data-loader for UserOverTime report

--- src/loaders/UseOverTimeLoader.js
+++ src/loaders/UseOverTimeLoader.js
@@ -1,5 +1,47 @@
-function UseOverTimeLoader() {
-  return <p>This is the <b>Use Over Time</b> report.</p>;
+import PropTypes from 'prop-types';
+import { stripesConnect } from '@folio/stripes/core';
+import UseOverTime from '../reports/UseOverTime';
+
+
+function UseOverTimeLoader({ data, resources }) {
+  return <UseOverTime
+    hasLoaded={resources.useOverTime.hasLoaded}
+    data={{
+      ...data,
+      useOverTime: resources.useOverTime.records,
+    }}
+  />;
 }
 
-export default UseOverTimeLoader;
+
+UseOverTimeLoader.manifest = {
+  useOverTime: {
+    type: 'okapi',
+    path: 'eusage-reports/stored-reports/use-over-time',
+    params: (_q, _p, _r, _l, props) => {
+      const aId = props.data.agreement.id;
+      if (!aId) return null;
+      return ({
+        agreementId: aId,
+        startDate: '1970-01-01', // XXX set from UI
+        endDate: '2999-12-31', // XXX set from UI
+      });
+    },
+  },
+};
+
+
+UseOverTimeLoader.propTypes = {
+  data: PropTypes.object.isRequired,
+  resources: PropTypes.shape({
+    useOverTime: PropTypes.shape({
+      hasLoaded: PropTypes.bool.isRequired,
+      records: PropTypes.arrayOf(
+        PropTypes.object.isRequired,
+      ),
+    }),
+  }).isRequired,
+};
+
+
+export default stripesConnect(UseOverTimeLoader);
