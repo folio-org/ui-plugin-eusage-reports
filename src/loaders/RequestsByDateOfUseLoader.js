db942b97bc25d018c43e077875d26082e944570e
Mike Taylor <mike@miketaylor.org.uk>
2021-08-27 14:41:01 +0100

    RequestsByDateOfUseLoader uses correct view component

--- src/loaders/RequestsByDateOfUseLoader.js
+++ src/loaders/RequestsByDateOfUseLoader.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
-import RequestsByPublicationYear from '../reports/RequestsByPublicationYear';
+import RequestsByDateOfUse from '../reports/RequestsByDateOfUse';
 import RequestsLoader from './RequestsLoader';
 
 function RequestsByDateOfUseLoader({ params, data }) {
-  return <RequestsLoader params={params} data={data} DisplayComponent={RequestsByPublicationYear} />;
+  return <RequestsLoader params={params} data={data} DisplayComponent={RequestsByDateOfUse} />;
 }
 
 RequestsByDateOfUseLoader.propTypes = {
