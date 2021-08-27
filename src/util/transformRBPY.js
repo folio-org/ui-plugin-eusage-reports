6898c09b88722f59d193fa7c6322cf5d73bbc21e
Mike Taylor <mike@indexdata.com>
2021-08-27 23:25:53 +0100

    Generate correct graph for Request by Publication Year

--- src/util/transformRBPY.js
+++ src/util/transformRBPY.js
@@ -33,23 +33,18 @@ function chooseColor(index, method) {
 }
 
 
-// We want three columns, for the three unique periodOfUse values 2019, 2020 and 2021.
-// Each column needs 10 stacked elements for the ten publication years.
-// So `datasets` must be an array of 10 sets, each of three elements.
+// We want ten columns, for the ten accessCountsByPeriod values
+// (really publication years, delivered in a field with a misleading
+// name), 2010-2019.
+// Each column needs three stacked elements for the three periods of use.
+// So `datasets` must be an array of three sets, each of 10 elements.
 
 function transformReqByPubYearData(rbpy, metricType) {
   if (!rbpy) return null;
-  const periodOfUseRegister = {};
-  rbpy.items.forEach(item => {
-    periodOfUseRegister[item.periodOfUse] = true;
-  });
-
-  const periodOfUseList = Object.keys(periodOfUseRegister).sort();
 
   const dataForPeriod = {};
   rbpy.items.forEach(item => {
     if (item.metricType === metricType) {
-      // if (item.accessType === 'OA_Gold') item.accessCountsByPeriod[0] = 123; // XXX no
       if (!dataForPeriod[item.periodOfUse]) {
         dataForPeriod[item.periodOfUse] = item.accessCountsByPeriod;
       } else {
@@ -58,19 +53,15 @@ function transformReqByPubYearData(rbpy, metricType) {
     }
   });
 
-  const dataForPeriodList = Object.keys(dataForPeriod).sort().map(periodOfUse => dataForPeriod[periodOfUse]);
-
-  const datasets = rbpy.accessCountPeriods.map((publicationYear, index) => {
-    return {
-      label: publicationYear,
-      data: dataForPeriodList.map(countByPublicationYear => countByPublicationYear[index]),
-      backgroundColor: chooseColor(index),
-      stack: 'Stack 0',
-    };
-  });
+  const datasets = Object.keys(dataForPeriod).sort().map((periodOfUse, index) => ({
+    label: periodOfUse,
+    data: dataForPeriod[periodOfUse],
+    backgroundColor: chooseColor(index),
+    stack: 'Stack 0',
+  }));
 
   return {
-    labels: periodOfUseList,
+    labels: Object.keys(dataForPeriod).sort(),
     datasets,
   };
 }
