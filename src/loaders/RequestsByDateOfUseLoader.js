4321c1e95de43652d901283b920f34e950843d6a
Mike Taylor <mike@miketaylor.org.uk>
2021-09-13 16:57:39 +0100

    Refactor
    Each of RequestsByDateOfUseLoader.js and
    RequestsByPublicationYearLoader.js is now standalone, and neither
    depends on RequestsLoader.js, which was previously the heart of both
    of them. This is preparatory to diverging these components to diverge
    from shared use of the same underlying WSAPI.

--- src/loaders/RequestsByDateOfUseLoader.js
+++ src/loaders/RequestsByDateOfUseLoader.js
@@ -1,14 +1,71 @@
 import PropTypes from 'prop-types';
+import { stripesConnect } from '@folio/stripes/core';
 import RequestsByDateOfUse from '../reports/RequestsByDateOfUse';
-import RequestsLoader from './RequestsLoader';
 
-function RequestsByDateOfUseLoader({ params, data }) {
-  return <RequestsLoader params={params} data={data} DisplayComponent={RequestsByDateOfUse} />;
+
+function RequestsByDateOfUseLoader({ params, data, resources }) {
+  return <RequestsByDateOfUse
+    url={resources.requests.url}
+    hasLoaded={resources.requests.hasLoaded}
+    params={params}
+    data={{
+      ...data,
+      requestsByPublicationYear: resources.requests.records[0],
+    }}
+  />;
 }
+
+
+RequestsByDateOfUseLoader.manifest = {
+  requests: {
+    type: 'okapi',
+    path: 'eusage-reports/stored-reports/reqs-by-pub-year',
+    params: (_q, _p, _r, _l, props) => {
+      const aId = props.data.agreement.id;
+      if (!aId) return null;
+      const res = {
+        agreementId: aId,
+        startDate: props.params.startDate,
+        endDate: props.params.endDate,
+        includeOA: props.params.includeOA,
+        accessCountPeriod: props.params.accessCountPeriod,
+      };
+
+      if (props.params.report === 'rbu') {
+        res.yopInterval = props.params.yopInterval;
+        res.periodOfUse = '1Y'; // XXX currently required by server
+      } else if (props.params.report === 'rbp') {
+        res.periodOfUse = props.params.periodOfUse;
+      }
+
+      return res;
+    },
+  },
+};
+
 
 RequestsByDateOfUseLoader.propTypes = {
   data: PropTypes.object.isRequired,
-  params: PropTypes.object.isRequired,
+  params: PropTypes.shape({
+    report: PropTypes.string.isRequired, // uot, rbu, etc.
+    // format: PropTypes.string.isRequired, // j=journal, b=book, etc.
+    includeOA: PropTypes.bool.isRequired,
+    startDate: PropTypes.string.isRequired, // ISO-format date
+    endDate: PropTypes.string.isRequired, // ISO-format date
+    accessCountPeriod: PropTypes.string.isRequired,
+    yopInterval: PropTypes.string.isRequired,
+    periodOfUse: PropTypes.string.isRequired,
+  }).isRequired,
+  resources: PropTypes.shape({
+    requests: PropTypes.shape({
+      url: PropTypes.string, // Not .isRequired, as this is briefly undefined
+      hasLoaded: PropTypes.bool.isRequired,
+      records: PropTypes.arrayOf(
+        PropTypes.object.isRequired,
+      ),
+    }),
+  }).isRequired,
 };
 
-export default RequestsByDateOfUseLoader;
+
+export default stripesConnect(RequestsByDateOfUseLoader);
--- src/loaders/RequestsByPublicationYearLoader.js
+++ src/loaders/RequestsByPublicationYearLoader.js
@@ -1,14 +1,71 @@
 import PropTypes from 'prop-types';
+import { stripesConnect } from '@folio/stripes/core';
 import RequestsByPublicationYear from '../reports/RequestsByPublicationYear';
-import RequestsLoader from './RequestsLoader';
 
-function RequestsByPublicationYearLoader({ params, data }) {
-  return <RequestsLoader params={params} data={data} DisplayComponent={RequestsByPublicationYear} />;
+
+function RequestsByPublicationYearLoader({ params, data, resources }) {
+  return <RequestsByPublicationYear
+    url={resources.requests.url}
+    hasLoaded={resources.requests.hasLoaded}
+    params={params}
+    data={{
+      ...data,
+      requestsByPublicationYear: resources.requests.records[0],
+    }}
+  />;
 }
+
+
+RequestsByPublicationYearLoader.manifest = {
+  requests: {
+    type: 'okapi',
+    path: 'eusage-reports/stored-reports/reqs-by-pub-year',
+    params: (_q, _p, _r, _l, props) => {
+      const aId = props.data.agreement.id;
+      if (!aId) return null;
+      const res = {
+        agreementId: aId,
+        startDate: props.params.startDate,
+        endDate: props.params.endDate,
+        includeOA: props.params.includeOA,
+        accessCountPeriod: props.params.accessCountPeriod,
+      };
+
+      if (props.params.report === 'rbu') {
+        res.yopInterval = props.params.yopInterval;
+        res.periodOfUse = '1Y'; // XXX currently required by server
+      } else if (props.params.report === 'rbp') {
+        res.periodOfUse = props.params.periodOfUse;
+      }
+
+      return res;
+    },
+  },
+};
+
 
 RequestsByPublicationYearLoader.propTypes = {
   data: PropTypes.object.isRequired,
-  params: PropTypes.object.isRequired,
+  params: PropTypes.shape({
+    report: PropTypes.string.isRequired, // uot, rbu, etc.
+    // format: PropTypes.string.isRequired, // j=journal, b=book, etc.
+    includeOA: PropTypes.bool.isRequired,
+    startDate: PropTypes.string.isRequired, // ISO-format date
+    endDate: PropTypes.string.isRequired, // ISO-format date
+    accessCountPeriod: PropTypes.string.isRequired,
+    yopInterval: PropTypes.string.isRequired,
+    periodOfUse: PropTypes.string.isRequired,
+  }).isRequired,
+  resources: PropTypes.shape({
+    requests: PropTypes.shape({
+      url: PropTypes.string, // Not .isRequired, as this is briefly undefined
+      hasLoaded: PropTypes.bool.isRequired,
+      records: PropTypes.arrayOf(
+        PropTypes.object.isRequired,
+      ),
+    }),
+  }).isRequired,
 };
 
-export default RequestsByPublicationYearLoader;
+
+export default stripesConnect(RequestsByPublicationYearLoader);
