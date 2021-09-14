100a24d022da1d62bb637fdc1704c2a11424d35f
Mike Taylor <mike@miketaylor.org.uk>
2021-09-14 18:20:14 +0100

    Complete rewrite to use totalRequestsPeriodsOfUseByPeriod
    Now we can ignore the `items`.
    
    Fixes the second half of UIPER-62.

--- src/util/transformRBPY.js
+++ src/util/transformRBPY.js
@@ -1,42 +1,53 @@
-import vectorAdd from './vectorAdd';
 import chooseColor from './chooseColor';
 
 
-// When we make a request with start- and end-dates 2019-2021, and get
-// back data showing that papers published in 15 different years were
-// used, we need fifteen columns (one per publication year), each with
-// three colours (one per usage year). This means we need three data-sets
-// of 15 points each.
+// The incoming data structure is:
+//
+// * accessCountPeriods is a list of publication periods (NOT access
+//   periods)
+// * totalRequestsPeriodsOfUseByPeriod is a list, parallel to
+//   accessCountPeriods, in which each element is a hash mapping
+//   usage-period to total request count within that period.
+// * uniqueRequestsPeriodsOfUseByPeriod is the same for unique requests
+//
+// (There are other elements but they are of no interest to us.)
+//
 
 function transformReqByPubYearData(rbpy, metricType) {
   if (!rbpy) return null;
 
-  const periodOfUseRegister = {};
-  rbpy.items.forEach(item => {
-    if (item.metricType === metricType) {
-      periodOfUseRegister[item.periodOfUse] = true;
-    }
+  let dataByPub;
+  if (metricType === 'Total_Item_Requests') {
+    dataByPub = rbpy.totalRequestsPeriodsOfUseByPeriod;
+  } else if (metricType === 'Unique_Item_Requests') {
+    dataByPub = rbpy.uniqueRequestsPeriodsOfUseByPeriod;
+  } else {
+    // eslint-disable-next-line no-console
+    console.error('transformReqByPubYearData: unsuported metricType =', metricType);
+    return null;
+  }
+
+  // dataByPub is a list, parallel to rbpy.accessCountPeriods, in
+  // which each entry is a hash mapping a usage period year to a
+  // count. But some of the hashes can, I guess, in general can omit
+  // some years. So -- *sigh* -- we have to walk all of the hashes to
+  // determine the complete list of usage periods, and put them in
+  // order.
+
+  const usagePeriodMap = {};
+  dataByPub.forEach(period => {
+    Object.keys(period).forEach(usagePeriod => {
+      usagePeriodMap[usagePeriod] = true;
+    });
   });
+  const usagePeriods = Object.keys(usagePeriodMap).sort();
 
-  const periodsOfUse = Object.keys(periodOfUseRegister).sort();
-
-  const periodOfUseIndex = {};
-  periodsOfUse.forEach((period, i) => { periodOfUseIndex[period] = i; });
-
-  const datasets = [];
-  rbpy.items.forEach(item => {
-    const pIndex = periodOfUseIndex[item.periodOfUse];
-    if (!datasets[pIndex]) {
-      datasets[pIndex] = {
-        label: item.periodOfUse,
-        data: item.accessCountsByPeriod,
-        backgroundColor: chooseColor(pIndex),
-        stack: 'Stack 0',
-      };
-    } else {
-      datasets[pIndex].data = vectorAdd(datasets[pIndex].data, item.accessCountsByPeriod);
-    }
-  });
+  const datasets = usagePeriods.map((usagePeriod, index) => ({
+    label: usagePeriod,
+    data: dataByPub.map(publicationYear => publicationYear[usagePeriod]),
+    backgroundColor: chooseColor(index),
+    stack: 'Stack 0',
+  }));
 
   return {
     labels: rbpy.accessCountPeriods,
