d23ef7b517d8030c08ac99eac4fccba175013c3d
Mike Taylor <mike@miketaylor.org.uk>
2021-09-21 16:36:23 +0100

    Do not display old reportStatus when new one is a 404
    This seems to be a bug in stripes-connect: once a resource has
    successfully fetched data, a subsequent failed fetch (404 Not Found in
    this case) results in the resource object's `records` element still
    containing the records from the old, successful fetch.
    
    Rather than leaping into stripes-connect and trying to fix this,
    instead I am directly checking the `failed` element of the resource
    object, and when it is set pretending that the record has not loaded,
    thereby displaying "(undefined)" in the UI.
    
    We may need to revisit this down the line if we need to distinguish
    between "not yet known" because the resource hasn't loaded yet and
    "definitely not set" because it's loaded but failed with a 404. But
    since the former is a transitory state it doesn't seem worth worrying
    about too much.

--- src/loaders/EusageVisualizationLoader.js
+++ src/loaders/EusageVisualizationLoader.js
@@ -5,9 +5,14 @@ import EusageVisualization from '../views/EusageVisualization';
 
 function EusageVisualizationLoader({ data, resources }) {
   return <EusageVisualization
-    lastUpdatedHasLoaded={resources.reportStatus.hasLoaded}
+    lastUpdatedHasLoaded={resources.reportStatus.hasLoaded && !resources.reportStatus.failed}
     data={{
       ...data,
+      // Once any agreement's status has been successfully fetched,
+      // stripes-connect apparently continues to include its content
+      // in the result of subsequent unsuccessful fetches. To avoid
+      // reporting old good records when we should be getting new
+      // failures, we have to explicitly check for failure
       reportStatus: resources.reportStatus.records[0],
     }}
   />;
@@ -32,6 +37,10 @@ EusageVisualizationLoader.propTypes = {
   resources: PropTypes.shape({
     reportStatus: PropTypes.shape({
       hasLoaded: PropTypes.bool.isRequired,
+      failed: PropTypes.oneOfType([
+        PropTypes.bool.isRequired,
+        PropTypes.object.isRequired,
+      ]).isRequired,
       records: PropTypes.arrayOf(
         PropTypes.object.isRequired,
       ),
