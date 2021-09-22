628026e7370873095c76792e360fbc6805e98d55
Mike Taylor <mike@miketaylor.org.uk>
2021-09-22 15:11:42 +0100

    Reorder jest.config.js
    Directives now appear in sections corresponding to those in
    doc/testing-with-jest-and-rtl.md

--- jest.config.js
+++ jest.config.js
@@ -4,25 +4,36 @@ const path = require('path');
 const esModules = ['@folio', 'ky'].join('|');
 
 module.exports = {
+  // Testing basics
+  testMatch: ['**/src/**/?(*.)test.{js,jsx}'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  reporters: ['jest-junit', 'default'], // XXX try this different ways
+
+  // Support for ES6
+  transform: { '^.+\\.(js|jsx)$': path.join(__dirname, './test/jest/jest-transformer.js') },
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
+
+  // Reporting test coverage
   collectCoverageFrom: [
     'src/**/*.{js,jsx}',
     '!test/**',
     '!**/node_modules/**',
   ],
-  coverageDirectory: './artifacts/coverage-jest/',
   coverageReporters: ['lcov', 'text'],
-  reporters: ['jest-junit', 'default'], // XXX try this different ways
-  transform: { '^.+\\.(js|jsx)$': path.join(__dirname, './test/jest/jest-transformer.js') },
-  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
-  moduleNameMapper: {
-    '^.+\\.(css)$': 'identity-obj-proxy',
-    '^.+\\.(svg)$': 'identity-obj-proxy',
-  },
-  testMatch: ['**/src/**/?(*.)test.{js,jsx}'],
-  testPathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: './artifacts/coverage-jest/',
+
+/*
+  // Support for DOM assertions
+  setupFilesAfterEnv: [path.join(__dirname, './test/jest/jest.setup.js')],
+
+  // Establishing mocks
   setupFiles: [
     path.join(__dirname, './test/jest/setupTests.js'),
     'jest-canvas-mock'
   ],
-  setupFilesAfterEnv: [path.join(__dirname, './test/jest/jest.setup.js')],
+  moduleNameMapper: {
+    '^.+\\.(css)$': 'identity-obj-proxy',
+    '^.+\\.(svg)$': 'identity-obj-proxy',
+  },
+*/
 };
