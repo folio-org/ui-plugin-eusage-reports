8e9e28f4389e221e9261939423343a75008e398d
Mike Taylor <mike@indexdata.com>
2021-09-23 22:35:59 +0100

    Mocking for useOkapiKy

--- src/views/MatchingSummary.test.js
+++ src/views/MatchingSummary.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { IntlProvider } from 'react-intl';
-import { cleanup, render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { useOkapiKy } from '@folio/stripes/core';
+import { cleanup, render, screen, fireEvent } from '@testing-library/react';
+import { useOkapiKy, CalloutContext } from '@folio/stripes/core';
 import reportTitles from '../../test/jest/data/reportTitles';
 import MatchingSummary from './MatchingSummary';
 import rawTranslations from '../../translations/ui-plugin-eusage-reports/en';
@@ -12,32 +12,52 @@ Object.keys(rawTranslations).forEach(key => {
 });
 
 
+// Empirically, this has to be done at the top level, not within the test. No-one knows why
+// See https://folio-project.slack.com/archives/C210UCHQ9/p1632425791183300?thread_ts=1632350696.158900&cid=C210UCHQ9
+useOkapiKy.mockReturnValue({
+  post: (_path) => {
+    // console.log('*** mocked okapiKy POST to', _path);
+    return new Promise((resolve, _reject) => {
+      // console.log('*** mocked okapiKy resolving promise');
+      resolve({ status: 'ok' });
+    });
+  },
+});
+
+
 const renderMatchingSummary = () => {
   const queryData = { matchType: undefined };
+  const callout = {
+    sendCallout: (_calloutData) => {
+      // console.log('*** sendCallout:', _calloutData.message.props.id);
+    }
+  };
 
   return render(
-    <IntlProvider locale="en-US" messages={translations}>
-      <MatchingSummary
-        hasLoaded
-        data={{
-          query: queryData,
-          counterReports: [],
-          usageDataProvider: {
-            harvestingDate: '2021-09-22T20:26:29.995390',
-          },
-          reportTitles,
-          reportTitlesCount: 42,
-        }}
-        mutator={{
-          query: {
-            update: (newData) => Object.assign(queryData, newData),
-          },
-        }}
-        reloadReportTitles={
-          () => undefined
-        }
-      />
-    </IntlProvider>
+    <CalloutContext.Provider value={callout}>
+      <IntlProvider locale="en-US" messages={translations}>
+        <MatchingSummary
+          hasLoaded
+          data={{
+            query: queryData,
+            counterReports: [],
+            usageDataProvider: {
+              harvestingDate: '2021-09-22T20:26:29.995390',
+            },
+            reportTitles,
+            reportTitlesCount: 42,
+          }}
+          mutator={{
+            query: {
+              update: (newData) => Object.assign(queryData, newData),
+            },
+          }}
+          reloadReportTitles={
+            () => undefined
+          }
+        />
+      </IntlProvider>
+    </CalloutContext.Provider>
   );
 };
 
@@ -58,7 +78,8 @@ describe('Matching Summary page', () => {
     expect(content).toBeVisible();
 
     // Harvesting date and status
-    // XXX See https://folio-project.slack.com/archives/C210UCHQ9/p1632414298168000?thread_ts=1632407596.164700&cid=C210UCHQ9
+    // XXX We should check for values associated with keys:
+    // See https://folio-project.slack.com/archives/C210UCHQ9/p1632414298168000?thread_ts=1632407596.164700&cid=C210UCHQ9
     expect(screen.getByText('9/22/2021')).toBeVisible(); // US formatting
     expect(screen.getByText('Pending review')).toBeVisible(); // Because some records are unmatched
 
@@ -70,23 +91,8 @@ describe('Matching Summary page', () => {
     expect(screen.getByRole('button')).toHaveTextContent('Update matches');
     expect(screen.getByRole('button')).toBeEnabled();
 
-    // See https://folio-project.slack.com/archives/C210UCHQ9/p1632425791183300?thread_ts=1632350696.158900&cid=C210UCHQ9
-    useOkapiKy.mockImplementation(() => {
-      console.log('*** in mocked useOkapiKy');
-      return () => {
-        console.log(' *** in mocked okapiKy');
-        return {
-          post: () => ({
-            json: () => ({
-              isLoading: false,
-              expenseClasses: [{ id: 'id', name: 'name' }],
-            }),
-          }),
-        };
-      };
-    });
-
     fireEvent.click(screen.getByRole('button'));
-    await waitFor(() => screen.getByText('Requested update'));
+    // XXX The callout mock renders nothing on screen for us to wait for
+    // await waitFor(() => screen.getByText('Requested update'));
   });
 });
